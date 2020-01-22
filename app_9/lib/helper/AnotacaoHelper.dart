d5d73efb37d43d16fd6dafcdd86983fb49bd42e1
JulioCCleto <44852279+JulioCCleto@users.noreply.github.com>
2020-01-21 21:48:55 -0300

    Anotações
    Em desenvolvimento...

--- app_9/lib/helper/AnotacaoHelper.dart
+++ app_9/lib/helper/AnotacaoHelper.dart
@@ -52,7 +52,15 @@ class AnotacaoHelper {
      );
     
     return resultado;
-
  }
+
+  recuperarAnotacoes() async {
+
+    var bancoDados = await db;
+    String sql = "SELECT * FROM $nomeTabela ORDER BY data DESC";
+    List anotacoes = await bancoDados.rawQuery(sql);
+    return anotacoes;
+  }
+
 }
 
--- app_9/lib/home.dart
+++ app_9/lib/home.dart
@@ -2,6 +2,8 @@ import 'package:app_9/helper/AnotacaoHelper.dart';
 import 'package:app_9/model/Anotacao.dart';
 import 'package:flutter/material.dart';
 
+import 'model/Anotacao.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -12,6 +14,9 @@ class _HomeState extends State<Home> {
   TextEditingController _descricaoController = TextEditingController();
 
   var _db = AnotacaoHelper();
+
+  List<Anotacao> anotacoes = List<Anotacao>();
+
 
   _exibirTelaCadastro() {
     showDialog(
@@ -26,17 +31,25 @@ class _HomeState extends State<Home> {
                   controller: _tituloController,
                   autofocus: true,
                   decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xFF961b1b), style: BorderStyle.solid),
+                      ),
                       labelText: "Título",
-                      labelStyle: TextStyle(
-                        fontSize: 22,
-                        color: Colors.grey[700]
-                      ),                    
+                      labelStyle:
+                          TextStyle(fontSize: 20, color: Colors.grey[700]),
                       hintText: "Digite o título..."),
                 ),
                 TextField(
                   controller: _descricaoController,
                   decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xFF961b1b), style: BorderStyle.solid),
+                      ),
                       labelText: "Descrição",
+                      labelStyle:
+                          TextStyle(fontSize: 20, color: Colors.grey[700]),
                       hintText: "Digite a descrição..."),
                 ),
               ],
@@ -68,6 +81,13 @@ class _HomeState extends State<Home> {
         });
   }
 
+  _recuperAnotacoes() async{
+
+    List anotacoesRecuperadas = await _db.recuperarAnotacoes();
+    print("Lista anotacoes: " + anotacoesRecuperadas.toString());
+
+  }
+
   _salvarAnotacao() async {
     //receber os dados dos forms, para salvar no banco de dados.
     //Criei um model com o construtor, passei como parametro na pagina AnotacaoHelper
@@ -77,13 +97,16 @@ class _HomeState extends State<Home> {
     //configurar a data
 
     Anotacao anotacao = Anotacao(titulo, descricao, DateTime.now().toString());
-    int resultado = await _db.salvarAnotacao( anotacao );
+    int resultado = await _db.salvarAnotacao(anotacao);
 
     print("Salvar anotação: " + resultado.toString());
   }
 
   @override
   Widget build(BuildContext context) {
+
+    _recuperAnotacoes();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(

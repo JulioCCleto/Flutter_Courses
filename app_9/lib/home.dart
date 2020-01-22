d3e0d0ade68d33487f7b0eaab8cf7098ea1a4981
JulioCCleto <44852279+JulioCCleto@users.noreply.github.com>
2020-01-22 15:29:34 -0300

    Anotações

--- app_9/lib/home.dart
+++ app_9/lib/home.dart
@@ -1,7 +1,8 @@
 import 'package:app_9/helper/AnotacaoHelper.dart';
 import 'package:app_9/model/Anotacao.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'model/Anotacao.dart';
 
 class Home extends StatefulWidget {
@@ -15,15 +16,29 @@ class _HomeState extends State<Home> {
 
   var _db = AnotacaoHelper();
 
-  List<Anotacao> anotacoes = List<Anotacao>();
+  List<Anotacao> _anotacoes = List<Anotacao>();
 
+  _exibirTelaCadastro({Anotacao anotacao}) {
+    String textoSalvarAtualizar = "";
 
-  _exibirTelaCadastro() {
+    if (anotacao == null) {
+      //Salvar
+
+      _tituloController.text = "";
+      _descricaoController.text = "";
+      textoSalvarAtualizar = "Salvar";
+    } else {
+      //Atualizar
+      _tituloController.text = anotacao.titulo;
+      _descricaoController.text = anotacao.descricao;
+      textoSalvarAtualizar = "Atualizar";
+    }
+
     showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text("Adicionar anotação"),
+            title: Text("$textoSalvarAtualizar anotação"),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -66,11 +81,11 @@ class _HomeState extends State<Home> {
               ),
               FlatButton(
                 onPressed: () {
-                  _salvarAnotacao();
+                  _salvarAtualizarAnotacao(anotacaoSelecionada: anotacao);
                   Navigator.pop(context);
                 },
                 child: Text(
-                  "Salvar",
+                  textoSalvarAtualizar,
                   style: TextStyle(
                     fontSize: 20,
                   ),
@@ -81,32 +96,74 @@ class _HomeState extends State<Home> {
         });
   }
 
-  _recuperAnotacoes() async{
-
+  _recuperAnotacoes() async {
     List anotacoesRecuperadas = await _db.recuperarAnotacoes();
-    print("Lista anotacoes: " + anotacoesRecuperadas.toString());
 
+    List<Anotacao> listaTemporaria = List<Anotacao>();
+
+    for (var item in anotacoesRecuperadas) {
+      Anotacao anotacao = Anotacao.fromMap(item);
+      listaTemporaria.add(anotacao);
+    }
+
+    setState(() {
+      _anotacoes = listaTemporaria;
+    });
+    listaTemporaria = null;
   }
 
-  _salvarAnotacao() async {
+  _salvarAtualizarAnotacao({Anotacao anotacaoSelecionada}) async {
     //receber os dados dos forms, para salvar no banco de dados.
     //Criei um model com o construtor, passei como parametro na pagina AnotacaoHelper
     String titulo = _tituloController.text;
-    String descricao = _tituloController.text;
+    String descricao = _descricaoController.text;
 
     //configurar a data
 
-    Anotacao anotacao = Anotacao(titulo, descricao, DateTime.now().toString());
-    int resultado = await _db.salvarAnotacao(anotacao);
+    if (anotacaoSelecionada == null) {
+      //salvar
 
-    print("Salvar anotação: " + resultado.toString());
+      Anotacao anotacao =
+          Anotacao(titulo, descricao, DateTime.now().toString());
+      int resultado = await _db.salvarAnotacao(anotacao);
+    } else {
+      // atualizar
+      anotacaoSelecionada.titulo = titulo;
+      anotacaoSelecionada.descricao = descricao;
+      anotacaoSelecionada.data = DateTime.now().toString();
+
+      int resultado = await _db.atualizarAnotacao(anotacaoSelecionada);
+    }
+
+    _tituloController.clear();
+    _descricaoController.clear();
+    _recuperAnotacoes();
+  }
+
+  _formartarData(String data) {
+    initializeDateFormatting("pt_BR");
+
+    var formater = DateFormat("dd/MM/yyyy HH:m");
+
+    DateTime dataConvertida = DateTime.parse(data);
+    String dataFormatada = formater.format(dataConvertida);
+
+    return dataFormatada;
+  }
+
+  _removerAnotacao(int id) async {
+    await _db.removerAnotacao(id);
+    _recuperAnotacoes();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperAnotacoes();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    _recuperAnotacoes();
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -128,7 +185,78 @@ class _HomeState extends State<Home> {
 
   _body() {
     return Column(
-      children: <Widget>[],
+      children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+            itemCount: _anotacoes.length,
+            itemBuilder: (context, index) {
+              final anotacao = _anotacoes[index];
+
+              return GestureDetector(
+                onLongPress: () {
+                  _exibirTelaCadastro(anotacao: anotacao);
+                },
+                child: Card(
+                  child: Dismissible(
+                    background: Container(
+                      color: Color(0xFF961b1b),
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) {
+                      _removerAnotacao(anotacao.id);
+                      setState(() {
+                        _anotacoes.removeAt(index);
+                      });
+                    },
+                    key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                    child: ListTile(
+                      title: Container(
+                        padding: EdgeInsets.only(bottom: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              anotacao.titulo,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "${_formartarData(anotacao.data)}",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      subtitle: Text(
+                        "${anotacao.descricao}",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
